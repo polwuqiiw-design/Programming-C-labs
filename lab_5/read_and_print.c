992a1d1af9fab53476adbe838a5c0926d6d40670
Настя <wuqiiw@MacBook-Air-Nasta.local>
2025-10-29 18:12:58 +0300

    Результаты выполненной первой задачи лабораторной 5

---
+++ lab_5/read_and_print.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void readAndPrintFile() {
+    FILE *fp = fopen("input.txt", "r");
+    if (fp == NULL) {
+        perror("Ошибка открытия файла");
+        exit(EXIT_FAILURE);
+    }
+
+    char buffer[256];
+    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+        printf("%s", buffer);
+    }
+
+    fclose(fp);
+}
+
+int main(void) {
+    readAndPrintFile();
+    return 0;
+}
